99a575872c9aa96c7318e03e545a17ba6e6f00cd
vccalvin33 <vc_calvin@yahoo.com>
2023-04-22 03:34:29 +0700

    add checkout session

--- src/pages/api/stripeCheckout.js
+++ src/pages/api/stripeCheckout.js
@@ -1,40 +1,52 @@
 import { loadStripe } from '@stripe/stripe-js';
 
 const PATH = 'http://localhost:8000'
+
+// const PATH = 'https://sentiport.de.r.appspot.com'
 export default class APIService {
-    static initStripe() {
-        console.log('checking out user')
+    // static initStripe() {
+    //     console.log('checking out user')
+    //     return fetch(`${PATH}/api/stripe-config/`, {
+    //         method: "GET",
+    //         // headers: {
+    //         //     Authorization: `Bearer ${token}`,
+    //         // },
+    //     }).then((result) => result.json())
+    //         .then((data) => {
+    //             // Initialize Stripe.js
+    //             console.log(data)
+    //             const stripe = loadStripe(data.publishable_key)
+    //             return stripe
+    //         });
+    // }
+
+    static checkoutUser() {
         return fetch(`${PATH}/api/stripe-config/`, {
             method: "GET",
             // headers: {
             //     Authorization: `Bearer ${token}`,
             // },
         }).then((result) => result.json())
-            .then((data) => {
+            .then(async (data) => {
                 // Initialize Stripe.js
                 console.log(data)
-                const stripe = loadStripe(data.publishable_key)
-                return stripe
-            });
-    }
-
-    static checkoutUser() {
-        const stripe = this.initStripe();
-        console.log(stripe)
-        fetch(`${PATH}/api/create-checkout-session/`)
-            .then((result) => { return result.json(); })
-            .then((data) => {
-                console.log(data);
-                // Redirect to Stripe Checkout
-                return stripe.redirectToCheckout({ sessionId: data.sessionId })
+                const stripe = await loadStripe(data.publishable_key)
+                console.log(stripe)
+                fetch(`${PATH}/api/create-checkout-session/`)
+                    .then((result) => { return result.json(); })
+                    .then((data) => {
+                        console.log(data);
+                        // Redirect to Stripe Checkout
+                        return stripe.redirectToCheckout({ sessionId: data.sessionId })
+                    })
+                    .then((res) => {
+                        console.log(res);
+                    });
             })
-            .then((res) => {
-                console.log(res);
-            });
     }
 
     static registerUser(body) {
-        return fetch(`${PATH}/api/users/`, {
+        return fetch(`${PATH} / api / users / `, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -49,7 +61,7 @@ export default class APIService {
     }
 
     static loginUser(body) {
-        return fetch(`${PATH}/api/token/`, {
+        return fetch(`${PATH} / api / token / `, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -64,7 +76,7 @@ export default class APIService {
     }
 
     static RequestPasswordEmail(body) {
-        return fetch(`${PATH}/api/request-reset-email/`, {
+        return fetch(`${PATH} / api / request - reset - email / `, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -79,7 +91,7 @@ export default class APIService {
     }
 
     static ResetPassword(body) {
-        return fetch(`${PATH}/api/password-reset-complete/`, {
+        return fetch(`${PATH} / api / password - reset - complete / `, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json",
@@ -94,7 +106,7 @@ export default class APIService {
     }
 
     static ChangePassword(body, token) {
-        return fetch(`${PATH}/api/change-password/`, {
+        return fetch(`${PATH} / api / change - password / `, {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json",
