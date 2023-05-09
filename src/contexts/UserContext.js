c2984d8bdd77ef16f3e68dd8fd464bb6ef09abcb
AurelliaChristie <aurelliachristie77@gmail.com>
2023-05-10 00:02:39 +0700

    fetch user data using new endpoint

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -3,22 +3,6 @@ import useLocalStorage from "use-local-storage";
 
 export const UserContext = createContext();
 
-const parseToken = (token) => {
-  const base64URL = token.split(".")[1];
-  const base64 = base64URL.replace(/-/g, "+").replace(/_/g, "/");
-  const jsonPayload = decodeURIComponent(
-    window
-      .atob(base64)
-      .split("")
-      .map((c) => {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join("")
-  );
-
-  return JSON.parse(jsonPayload);
-};
-
 export const UserContextWrapper = ({ children }) => {
   const [authTokens, setAuthTokens] = useLocalStorage("userSession", null);
   const [user, setUser] = useState(null);
@@ -52,8 +36,17 @@ export const UserContextWrapper = ({ children }) => {
       if (response.status === 200) {
         setAuthTokens(data);
         localStorage.setItem("userSession", JSON.stringify(data));
-        const userInfo = parseToken(data.access);
-        setUser(userInfo);
+        const userInfoResponse = await fetch(
+          `https://sentiport.de.r.appspot.com/api/credits/`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${data.access}`,
+            },
+          }
+        );
+        const userInfo = await userInfoResponse.json();
+        setUser(userInfo?.user);
         setLoggingIn(false);
       } else {
         setLoggingIn(false);
